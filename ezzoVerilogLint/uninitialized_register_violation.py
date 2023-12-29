554825857940013513d4d4354f8a15c1d20e400d
youssefalbertt <youssefalbert2003@gmail.com>
2023-12-29 23:56:13 +0200

    Fix check_uninitialized_registers function

--- ezzoVerilogLint/uninitialized_register_violation.py
+++ ezzoVerilogLint/uninitialized_register_violation.py
@@ -1,31 +1,47 @@
+from global_functions import get_declared_regs
 import re
 
 def check_uninitialized_registers(verilog_code):
-    lines = verilog_code.split('\n')
+
     uninitialized_registers = []
+    initialized_regs = []
+    declared_registers = get_declared_regs(verilog_code)
+    
+    for each_reg in declared_registers:
+        initialized_regs.append({"name":each_reg['name'] , "loc": re.finditer(r'\b' + each_reg["name"] + r'\s*<?=', verilog_code)})
 
-    for i, line in enumerate(lines, start=1):
-        # Check for lines containing reg or wire declarations
-        if re.search(r'\b(?:reg|wire)\s+(\[\d+:\d+\])?\s*(\w+)\s*;', line):
-            # Extract the register name
-            match = re.search(r'\b(?:reg|wire)\s+(\[\d+:\d+\])?\s*(\w+)\s*;', line)
-            if match:
-                register_name = match.group(2)
-
-                if not re.search(r'\b' + register_name + r'\s*=', line):
-                    for j, line in enumerate(lines, start=i + 1):
-                        if re.search(r'\b' + register_name + r'\s*=', line):
-                            break
-                    else: uninitialized_registers.append((j, register_name))
-
+    appeared_regs = re.finditer(r"=\s*?(?P<left_operand>[a-zA-Z]\w*)(\s*?[+*-]\s*?(?P<right_operand>[a-zA-Z]\w*))?" , verilog_code)
+    for appeared_reg in appeared_regs:
+        found = False
+        for initialized_reg in initialized_regs:
+            if appeared_reg.group("left_operand") == initialized_reg["name"]:
+                found = True
+                for loc in initialized_reg["loc"]:
+                    init = False
+                    if loc.start() < appeared_reg.start():
+                        init = True
+                        break
+                if not init:
+                    uninitialized_registers.append((verilog_code.count("\n", 0, appeared_reg.start()) , appeared_reg.group("left_operand")))
+            if appeared_reg.group("right_operand" ) == initialized_reg["name"]:
+                found = True
+                for loc in initialized_reg["loc"]:
+                    init= False
+                    if loc.start() < appeared_reg.start():
+                        init = True
+                        break
+                if not init:
+                    uninitialized_registers.append((verilog_code.count("\n", 0, appeared_reg.start()) , appeared_reg.group("right_operand")))
+        if not found:
+            uninitialized_registers.append((verilog_code.count("\n", 0,  appeared_reg.start()) , appeared_reg.group("left_operand")))
     return uninitialized_registers
 
 if __name__ == "__main__":
     # Example usage:
     verilog_code = """
-    reg [7:0] reg1;
+    reg [7:0] reg1, reg2 , reg3;
     wire [3:0] result ;
-    result = 4'b0101;
+    result <= 4'b0101;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             reg1 <= 8'b00000000;
@@ -42,6 +58,7 @@ if __name__ == "__main__":
     always @(posedge clk) begin
         // reg3 is uninitialized in this block
         data_out <= result + reg3;
+        reg3 = 8'b01010101
     end
     """
 
