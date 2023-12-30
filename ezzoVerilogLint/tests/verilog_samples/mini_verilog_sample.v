34a4cbf6c2935c5ad60e1f0ad1c0c2e0a719ef6c
mohaemdwalid <mohamedwalid680@gmail.com>
2023-12-30 15:00:26 +0200

    Updated testing

--- ezzoVerilogLint/tests/verilog_samples/mini_verilog_sample.v
+++ ezzoVerilogLint/tests/verilog_samples/mini_verilog_sample.v
@@ -47,3 +47,29 @@ if(reg2)
 f = 2'b01;
 else
 f = 2'b10;
+
+
+  input reg [3:0] reg_1;
+  output reg [3:0] reg_2;
+  input wire [1:0] reg_3;
+  reg [3:0] reg_4;
+  reg [4:0] reg_5;
+
+    // Test 1: Arithmetic overflow with addition
+    reg_1 = 8;
+    reg_4 = 7;
+    reg_5 = reg_4 + reg_1;  // No Overflow here
+
+    // Test 2: Arithmetic overflow with subtraction
+    reg_1 = 5;
+    reg_2 = 7;
+    reg_2 = reg_5 - reg_2;  // Overflow here
+
+
+    // Test 3: No overflow
+    reg_3 = 2;
+    reg_2 = 3;
+    reg_3 = reg_5 + reg_2;  //  overflow
+
+
+
