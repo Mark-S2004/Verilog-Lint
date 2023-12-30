ae4f2daabe7cd7ce94357e1a978685b50fccdc2d
Mark Saleh <mark-saleh@outlook.com>
2023-12-30 21:01:54 +0200

    Fix multidriven register violation module

--- ezzoVerilogLint/multidriven_register_violation.py
+++ ezzoVerilogLint/multidriven_register_violation.py
@@ -1,80 +1,34 @@
 import re
 from global_functions import get_declared_regs
+
+
+def find_occurrences(string, register_name):
+    pattern = rf"\b{re.escape(register_name)}\s*?<=\s*"
+    return [match.start() for match in re.finditer(pattern, string)]
+
+
 def check_multidriven_registers(verilog_code):
     # Extract register declarations
-  
     registers = get_declared_regs(verilog_code)
-    
 
     # Extract always blocks
-    always_blocks = re.findall(r'\balways\b\s*?@\s*?\(.*?\).*?\bend\b', verilog_code, re.DOTALL)
-    
+    always_blocks = re.findall(
+        r"\balways\b\s*?@\s*?\(.*?\).*?\bend\b", verilog_code, re.DOTALL
+    )
 
-
-
-
-
+    multidriven_registers = []
 
     # Check each register in the always blocks
     for register in registers:
-        assignments = []
+        occurrences = []
+
         for block in always_blocks:
             if register["name"] in block:
-                assign_stmts = re.findall(r'\b(\w+)\s*?<=', block)
-                assignments.extend(assign_stmts)
-                
-    print("assignments",assignments)
-    seen = set()
-    duplicates = set()
+                occurrences.extend(find_occurrences(verilog_code, register["name"]))
 
-    for item in assignments:
-        if item in seen:
-            duplicates.add(item)
-        seen.add(item)
+        if len(set(occurrences)) > 1:
+            for occurrence in set(occurrences):
+                line_number = verilog_code.count("\n", 0, occurrence) + 1
+                multidriven_registers.append((register["name"], line_number))
 
-    
-    
-
-    return list(duplicates)
-
-
-# Example usage
-verilog_code = """
-module example;
-  reg a, b, c, d, e;
-
-  always @(posedge clk)
-  begin
-    a <= b;
-  end
-  always @(posedge clk)
-  begin
-    b <= c;
-  end
-  always @(posedge clk)
-  begin
-    c <= d;
-  end
-  always @(posedge clk)
-  begin
-    b <= d;
-  end
-  always @(posedge clk)
-  begin
-    d <= e;
-  end
-  always @(posedge clk)
-  begin
-    a <= e;
-  end
-
-  always @(posedge clk)
-  begin
-    e <= c;
-  end
-
-endmodule
-"""
-
-multidriven_registers = check_multidriven_registers(verilog_code)
-print("Multidriven registers:", multidriven_registers)
+    return multidriven_registers
