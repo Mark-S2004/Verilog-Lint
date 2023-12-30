964aa6a3a93976711e20f088f4d24ff55875c6c5
Mark Saleh <mark-saleh@outlook.com>
2023-12-30 17:31:19 +0200

    Implement Veriloglint and Report classes
    Veriloglint is the package's main class

--- ezzoVerilogLint/Report.py
+++ ezzoVerilogLint/Report.py
@@ -3,14 +3,6 @@ This module implements Report class
 """
 
 
-import arithmetic_overflow_violation
-import case_violations
-import infer_latch_violation
-import multidriven_register_violation
-import uninitialized_register_violation
-import global_functions
-
-
 class Report:
     def __init__(self):
         self.arithmetic_overflow_violations = []
@@ -40,20 +32,19 @@ class Report:
             for violation in self.arithmetic_overflow_violations:
                 report += f"\nArithmetic overflow violation on line number {violation[1]} for '{violation[0]}' register"
 
-
         if not len(self.fullcase_violations):
             report += "\nNo non full-case violation"
         else:
             for line_numbers in self.fullcase_violations:
                 report += f"\nNon full case violation on line number {line_numbers} "
-        
 
         if not len(self.parallelcase_violations):
             report += "\nNo non parallel case violation"
         else:
             for line_numbers in self.parallelcase_violations:
-                report += f"\nNon parallel case violation on line number {line_numbers} "
-
+                report += (
+                    f"\nNon parallel case violation on line number {line_numbers} "
+                )
 
         if not len(self.infer_latch_violations):
             report += "\nNo latches infered"
@@ -61,20 +52,16 @@ class Report:
             for line_numbers in self.infer_latch_violations:
                 report += f"\nInfer latch violation on line number {line_numbers} "
 
-
         if not len(self.multidriven_register_violations):
             report += "\nNo multidriven register violation"
         else:
             for duplicate in self.multidriven_register_violations:
                 report += f"\nMultidriven register violation {list(duplicate)}"
 
-     
-
         if not len(self.uninitialized_register_violations):
             report += "\nNo uninitialized register violation"
         else:
             for uninitialized_registers in self.uninitialized_register_violations:
                 report += f"\nUninitialized register found in line {uninitialized_registers[0]} for register {uninitialized_registers[1]}"
-
 
         return report
---
+++ ezzoVerilogLint/Veriloglint.py
@@ -0,0 +1,67 @@
+"""
+This module implements Veriloglint class
+"""
+import os
+from Report import Report
+from arithmetic_overflow_violation import detect_arithmetic_overflow
+from case_violations import catch_non_full_case, catch_non_parallel_case
+from infer_latch_violation import check_infer_latch
+from multidriven_register_violation import check_multidriven_registers
+from uninitialized_register_violation import check_uninitialized_registers
+
+
+class Veriloglint:
+    """Veriloglint class generates lint report and can
+
+    Attributes:
+        verilog_code (str): string of verilog module that was read from the file
+                            in the path specified
+        report (Report): A report that contains all violations found in the file and
+                            the line number on which they occurred
+    """
+
+    def __init__(self, verilog_module_path: str):
+        self._verilog_code = verilog_module_path
+        self._report = self._verilog_code
+
+    def __repr__(self) -> str:
+        return repr(self.__report)
+
+    @property
+    def verilog_code(self) -> str:
+        return self.__verilog_code
+
+    @verilog_code.setter
+    def _verilog_code(self, verilog_module_path):
+        with open(verilog_module_path, "r", encoding="utf-8") as v:
+            self.__verilog_code = v.read()
+
+    @property
+    def report(self) -> str:
+        return repr(self.__report)
+
+    @report.setter
+    def _report(self, verilog_code: str):
+        self.__report = Report()
+        self.__report.arithmetic_overflow_violations = detect_arithmetic_overflow(
+            verilog_code
+        )
+        self.__report.fullcase_violations = catch_non_full_case(verilog_code)
+        self.__report.parallelcase_violations = catch_non_parallel_case(verilog_code)
+        self.__report.infer_latch_violations = check_infer_latch(verilog_code)
+        self.__report.multidriven_register_violations = check_multidriven_registers(
+            verilog_code
+        )
+        self.__report.uninitialized_register_violations = check_uninitialized_registers(
+            verilog_code
+        )
+
+    def generate_report(self, path: str = f"{os.getcwd}/report.txt"):
+        """Generate a report in
+
+        Arguments:
+            path -- Path including filename to generate or overwrite report in.
+                    By defualt it is going to write in report.txt in the current working directory
+        """
+        with open(f"{path}", "w", encoding="utf-8") as report:
+            report.write(repr(self.__report))
