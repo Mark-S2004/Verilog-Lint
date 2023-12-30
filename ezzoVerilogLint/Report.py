57cafaa73549e28627092a367250b3768b28c20a
youssefalbertt <youssefalbert2003@gmail.com>
2023-12-30 17:20:21 +0200

    Add report class

--- ezzoVerilogLint/Report.py
+++ ezzoVerilogLint/Report.py
@@ -3,7 +3,12 @@ This module implements Report class
 """
 
 
-from ezzoVerilogLint import arithmetic_overflow_violation
+import arithmetic_overflow_violation
+import case_violations
+import infer_latch_violation
+import multidriven_register_violation
+import uninitialized_register_violation
+import global_functions
 
 
 class Report:
@@ -30,9 +35,46 @@ class Report:
         report = "Lint Report\n------------"
 
         if not len(self.arithmetic_overflow_violations):
-            report += "\nNo arithmetic overflow violations"
+            report += "\nNo arithmetic overflow violation"
         else:
             for violation in self.arithmetic_overflow_violations:
                 report += f"\nArithmetic overflow violation on line number {violation[1]} for '{violation[0]}' register"
+
+
+        if not len(self.fullcase_violations):
+            report += "\nNo non full-case violation"
+        else:
+            for line_numbers in self.fullcase_violations:
+                report += f"\nNon full case violation on line number {line_numbers} "
+        
+
+        if not len(self.parallelcase_violations):
+            report += "\nNo non parallel case violation"
+        else:
+            for line_numbers in self.parallelcase_violations:
+                report += f"\nNon parallel case violation on line number {line_numbers} "
+
+
+        if not len(self.infer_latch_violations):
+            report += "\nNo latches infered"
+        else:
+            for line_numbers in self.infer_latch_violations:
+                report += f"\nInfer latch violation on line number {line_numbers} "
+
+
+        if not len(self.multidriven_register_violations):
+            report += "\nNo multidriven register violation"
+        else:
+            for duplicate in self.multidriven_register_violations:
+                report += f"\nMultidriven register violation {list(duplicate)}"
+
+     
+
+        if not len(self.uninitialized_register_violations):
+            report += "\nNo uninitialized register violation"
+        else:
+            for uninitialized_registers in self.uninitialized_register_violations:
+                report += f"\nUninitialized register found in line {uninitialized_registers[0]} for register {uninitialized_registers[1]}"
+
 
         return report
